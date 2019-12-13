09cc085e1068102b26f38012b4f4a5fbdb6481bc
halfbax <mail@leon-spors.de>
2019-12-13 16:32:55 +0100

    experimental 'status' thread for solving socket alive problems

--- managementServer/classes/server.py
+++ managementServer/classes/server.py
@@ -8,13 +8,136 @@ import time
 class Server:
     sock = None
     clients = {}
-    clientTypeConnected = []
     workers = []
     
     data = None
-
     shutdownRequest = False
 
+    def request(self, request):
+        """[summary]
+        
+        Args:
+            request (Request): [description]
+        """
+        # TODO: Docstring
+        if request == Request.Shutdown:
+            self.shutdownRequest = True
+
+    def _start_(self):
+        """Handles incoming connections.
+        """
+        thread = threading.Thread(target=self._incomingConnectionHandler_)
+        thread.start()
+
+    def _incomingConnectionHandler_(self):
+        # TODO: Docstring
+        logging.debug("Awaiting connection requests")
+
+        while not self.shutdownRequest:
+            connection, addr = self.sock.accept()
+            logging.debug("Connection {0} etablished. Starting worker...".format(addr))
+
+            workerThread = threading.Thread(target=self._worker_, args=(connection,))
+            workerThread.start()
+
+    def recvall(self, sock, count):
+        # TODO: docstring
+        buf = b''
+        while count:
+            newbuf = sock.recv(count)
+            if not newbuf:
+                return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
+
+    def _recvImageWorker_(self):
+        logging.debug("recv image worker started")
+
+        client = self.clients["img"]
+
+        try:
+            while True:
+                length = self.recvall(client, 16)
+                stringData = self.recvall(client, int(length))
+
+                self.data = stringData
+        except:
+            self.clients["img"] = None
+
+    def _echoWorker_(self):
+        # TODO: Clean up
+        #   Docstring
+        logging.debug("echo worker started")
+
+        mlClient = self.clients.get("ml")
+
+        lastSend = 0
+        try:
+            while self.clients.get("ml") != None and self.clients.get("img") != None:
+                datac = self.data
+                if datac != None:
+                    currLen = len(datac)
+                    if lastSend != currLen:
+                        logging.info(f"Sent {currLen}")
+                        lastSend = currLen                    
+                        mlClient.send((str(lastSend).ljust(16)).encode("ascii"))
+                        mlClient.send(datac)
+        except Exception as e:
+            self.clients["ml"] = None
+            print(e)
+
+        logging.debug("echo worker stopped")
+
+    def _statusWorker_(self):
+        echoWorkerThread = None
+
+        while echoWorkerThread == None:
+            for w in self.workers:
+                if w.name == "echo_thread":
+                    echoWorkerThread = w
+            time.sleep(1)
+
+        while True:
+            if echoWorkerThread.isAlive():
+                for c in self.clients:
+                    if c == None:
+                        logging.debug(f"Stopping echo worker. Reason: {c.getsockname()} connection lost")
+                        echoWorkerThread.stop()        
+            else:
+                if self.clients.get("img") != None and self.clients.get("ml") != None:
+                    logging.debug(f"Start echo worker. All clients connected.")
+                    echoWorkerThread.start()
+
+            time.sleep(1)
+    
+    def _worker_(self, client):
+        # TODO: Encapsulate inner worker routine.
+        # Exception handling
+        # Clean up.
+        # Docstring
+        logging.debug("Worker started.")
+
+        ct = None
+
+        # Waiting for registration
+        data = self.recvall(client, 8).decode("ascii")
+        data = str(data).split()
+
+        if data[0] == "REG" and data[1] == "img":
+            logging.debug("Registration complete.")
+            self.clients["img"] = client
+            for w in self.workers:
+                if w.getName == "recvImage_thread":
+                    w.start()
+        elif data[0] == "REG" and data[1] == "ml":
+            self.clients["ml"] = client
+            logging.debug("Registration complete.")
+            pass
+        else:
+            logging.debug("Registration failed. Closing connection.")
+            client.close()
+            
     def __init__(self, addr):
         """Initializes the socket server.
             
@@ -31,105 +154,10 @@ class Server:
         self.sock.listen(True)
         self._start_()
 
-    def request(self, request):
-        """[summary]
-        
-        Args:
-            request (Request): [description]
-        """
-        # TODO: Docstring
-        if request == Request.Shutdown:
-            self.shutdownRequest = True
-
-    def _start_(self):
-        """Handles incoming connections.
-        """
-        thread = threading.Thread(target=self.__incomingConnectionHandler__)
-        thread.start()
-
-    def __incomingConnectionHandler__(self):
-        # TODO: Docstring
-        logging.debug("Awaiting connection requests")
-
-        while not self.shutdownRequest:
-            connection, addr = self.sock.accept()
-            logging.debug("Connection {0} etablished. Starting worker...".format(addr))
-
-            workerThread = threading.Thread(target=self.__worker__, args=(connection,))
-            workerThread.start()
-
-    def recvall(self, sock, count):
-        # TODO: docstring
-        buf = b''
-        while count:
-            newbuf = sock.recv(count)
-            if not newbuf:
-                return None
-            buf += newbuf
-            count -= len(newbuf)
-        return buf
-
-    def _recvImageWorker_(self, client):
-        logging.debug("recv image worker started")
-        while True:
-            length = self.recvall(client, 16)
-            stringData = self.recvall(client, int(length))
-            self.data = stringData
-            self.dataChanged = time.time()
-
-    def _echoWorker_(self):
-        # TODO: Clean up
-        #   Docstring
-        logging.debug("echo worker started")
-
-        # Find ml client
-        client = None
-        while client == None:
-            logging.info("lf")
-            client = self.clients.get("ml")
-            time.sleep(0.1)
-
-        logging.info("ml client found")
-
-        lastSend = 0
-        while True:
-            datac = self.data
-            currLen = len(datac)
-            if lastSend != currLen:
-                logging.info(f"Sent {currLen}")
-                lastSend = currLen
-                client.send((str(lastSend).ljust(16)).encode("ascii"))
-                client.send(datac)
-
-    def __worker__(self, client):
-        # TODO: Encapsulate inner worker routine.
-        # Exception handling
-        # Clean up.
-        # Docstring
-        logging.debug("Worker started.")
-
-        ct = None
-
-        # Waiting for registration
-        data = self.recvall(client, 8).decode("ascii")
-        data = str(data).split()
-
-        if data[0] == "REG" and data[1] == "img":
-            logging.debug("Registration complete.")
-            riw = threading.Thread(target=self._recvImageWorker_, args=(client,))
-            riw.start()
-            self.workers.append(riw)
-            self.clients["img"] = client
-
-            ew = threading.Thread(target=self._echoWorker_)
-            ew.start()
-            self.workers.append(ew)
-        elif data[0] == "REG" and data[1] == "ml":
-            self.clients["ml"] = client
-            logging.debug("Registration complete.")
-            pass
-        else:
-            logging.debug("Registration failed. Closing connection.")
-            client.close()
-            
-
+        logging.debug("Threads initialization...")
+        self.workers.append(threading.Thread(target=self._recvImageWorker_, args=(), name="recvImage_thread"))
+        #self.workers.append(threading.Thread(target=self._recvImageWorker_, args=(), name="recvPred_thread"))        
+        self.workers.append(threading.Thread(target=self._echoWorker_, args=(), name="echo_thread"))
+        t = threading.Thread(target=self._statusWorker_, args=(), name="status_thread")
+        t.start()
+        self.workers.append(t)
